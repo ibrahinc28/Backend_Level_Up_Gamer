ab13e7b09ac37ec4ace848cb0a1c070ef7331bb4
cesar <cesarmaldonadomecanicodae13@gmail.com>
2025-10-31 16:31:34 -0300

    estructura basica par el proyecto

---
+++ src/main/java/com/levelupgamer/backend/LevelUpGamerApplication.java
@@ -0,0 +1,13 @@
+package com.levelupgamer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpGamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelUpGamerApplication.class, args);
+	}
+
+}
