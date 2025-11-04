8c7ee1ed6afd0d0ef1dcd2b4fbc0fde212507100
cesar <cesarmaldonadomecanicodae13@gmail.com>
2025-11-03 23:30:09 -0300

    Creando backend producto y home

---
+++ src/main/java/com/levelupgamer/backend/model/Producto.java
@@ -0,0 +1,46 @@
+package com.levelupgamer.backend.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.GenerationType;
+
+
+@Entity
+@Table(name = "productos")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Producto {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "imagen")
+    private String imagen;
+
+    @Column(name = "codigo", nullable = false, unique = true)
+    private String codigo;
+
+    @Column (name = "categoria", nullable = false)
+    private String categoria;
+
+    @Column (name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "descripcion", nullable = false, length = 500)
+    private String descripcion;
+
+    @Column(name = "precio", nullable = false)
+    private Double precio;
+
+    @Column(name = "destacado")
+    private Boolean destacado;
+
+
+}
---
+++ src/main/java/com/levelupgamer/backend/repository/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.levelupgamer.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.levelupgamer.backend.model.Producto;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+    // List<Producto> findByCategoria(String categoria);
+}
