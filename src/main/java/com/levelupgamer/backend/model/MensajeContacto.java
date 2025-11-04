d15386db304380e06976f19168842b34e4cb93e9
Vania <Van.carvajal@duocuc.cl>
2025-11-04 18:09:21 -0300

    Mensaje de contacto

--- src/main/java/com/levelupgamer/backend/model/MensajeContacto.java
+++ src/main/java/com/levelupgamer/backend/model/MensajeContacto.java
@@ -1,5 +1,43 @@
 package com.levelupgamer.backend.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Mensaje de contacto")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MensajeContacto {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "subject", nullable = false)
+    private String subject;
+
+    @Column(name = "message", nullable = false, columnDefinition = "Text")
+    private String message;
+
+    @Column(name = "timeStramp", nullable = false)
+    private LocalDateTime timeStramp;
+
+    @Column(name = "status")
+    private String status;
 }
