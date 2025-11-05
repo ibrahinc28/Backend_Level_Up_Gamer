bb5a1ce0d37652197533df63ae9c6db19d0e77de
Vania <Van.carvajal@duocuc.cl>
2025-11-05 02:27:07 -0300

    MensajeContactoController

--- src/main/java/com/levelupgamer/backend/controller/MensajeContactoController.java
+++ src/main/java/com/levelupgamer/backend/controller/MensajeContactoController.java
@@ -1,5 +1,15 @@
 package com.levelupgamer.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.levelupgamer.backend.service.MensajeContactoService;
+
+@RestController
+@RequestMapping("/api/contactos")
 public class MensajeContactoController {
     
+    @Autowired
+    private MensajeContactoService mensajeContactoService;
 }
