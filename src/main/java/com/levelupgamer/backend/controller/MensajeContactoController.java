ebd695170cf884701bc9d8a93e78ba4fa37f67bc
Vania <Van.carvajal@duocuc.cl>
2025-11-05 02:28:56 -0300

    MensajeController

--- src/main/java/com/levelupgamer/backend/controller/MensajeContactoController.java
+++ src/main/java/com/levelupgamer/backend/controller/MensajeContactoController.java
@@ -1,9 +1,14 @@
 package com.levelupgamer.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.levelupgamer.backend.model.MensajeContacto;
 import com.levelupgamer.backend.service.MensajeContactoService;
 
 @RestController
@@ -12,4 +17,10 @@ public class MensajeContactoController {
     
     @Autowired
     private MensajeContactoService mensajeContactoService;
+
+    @PostMapping
+    public ResponseEntity<MensajeContacto> crearMensaje(@RequestBody MensajeContacto mensaje){
+        MensajeContacto nuevoMensaje = mensajeContactoService.guardarMensaje(mensaje);
+        return new ResponseEntity<>(nuevoMensaje, HttpStatus.CREATED);
+    }
 }
