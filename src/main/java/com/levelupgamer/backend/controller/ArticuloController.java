17a54042199e9650860861ea7a22ec9f3a6eafaa
Vania <Van.carvajal@duocuc.cl>
2025-11-09 21:55:14 -0300

    Modificando del articuloController y articuloService

--- src/main/java/com/levelupgamer/backend/controller/ArticuloController.java
+++ src/main/java/com/levelupgamer/backend/controller/ArticuloController.java
@@ -3,15 +3,23 @@ package com.levelupgamer.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.levelupgamer.backend.model.Articulo;
 import com.levelupgamer.backend.service.ArticuloService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 @RestController
@@ -42,5 +50,27 @@ public class ArticuloController {
             .orElse(ResponseEntity.notFound().build());
 
         }
+
+        @PostMapping
+        public ResponseEntity<Articulo> crearArticulo(@RequestBody Articulo articulo){
+            Articulo nuevoArticulo = articuloService.guardarArticulo(articulo);
+            return new ResponseEntity<>(nuevoArticulo, HttpStatus.CREATED);
+        }
+
+        @PutMapping("/{id}")
+        public ResponseEntity<Articulo> actualizarArticulo(@PathVariable Long id, @RequestBody Articulo articulo){
+            Articulo actualizado = articuloService.actualizarArticulo(id, articulo);
+            if (actualizado != null){
+                return ResponseEntity.ok(actualizado);
+            }
+            return ResponseEntity.notFound().build();
+        }
+
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Articulo> eliminarArticulo(@PathVariable Long id){
+            articuloService.eliminarArticulo(id);
+            return ResponseEntity.noContent().build();
+        }
+        
     }
 
