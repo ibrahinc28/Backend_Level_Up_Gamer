7f9484501b8b57db118f02166c67624732f69d36
Vania <Van.carvajal@duocuc.cl>
2025-11-10 00:10:20 -0300

    Modificación service y controller de articulo

--- src/main/java/com/levelupgamer/backend/service/ArticuloService.java
+++ src/main/java/com/levelupgamer/backend/service/ArticuloService.java
@@ -2,18 +2,19 @@ package com.levelupgamer.backend.service;
 
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.levelupgamer.backend.repository.ArticuloRepository;
 import org.springframework.stereotype.Service;
 
 import com.levelupgamer.backend.model.Articulo;
-import com.levelupgamer.backend.repository.ArticuloRepository;
 
 @Service
 public class ArticuloService {
 
-    @Autowired
     private ArticuloRepository articuloRepository;
+
+    ArticuloService(ArticuloRepository articuloRepository) {
+        this.articuloRepository = articuloRepository;
+    }
 
     public List<Articulo> obtenerTodos(){
         return articuloRepository.findAll();
@@ -29,5 +30,27 @@ public class ArticuloService {
 
     public List<Articulo> buscarPorTitulo(String keyword) {
         return articuloRepository.findByTitleContainingOrderByDateDesc(keyword);
+    }
+
+    public Articulo guardarArticulo(Articulo articulo){
+        return articuloRepository.save(articulo);
+
+    }
+
+    public Articulo actualizarArticulo(Long id, Articulo detallesArticulo){
+        return articuloRepository.findById(id).map(articuloExistente ->{
+            articuloExistente.setTitle(detallesArticulo.getTitle());
+            articuloExistente.setContent(detallesArticulo.getContent());
+            articuloExistente.setType(detallesArticulo.getType());
+            articuloExistente.setDate(detallesArticulo.getDate());
+            articuloExistente.setImageSrc(detallesArticulo.getImageSrc());
+            articuloExistente.setSummary(detallesArticulo.getSummary());
+
+            return articuloRepository.save(articuloExistente);
+        }).orElse(null);
+    }
+
+    public void eliminarArticulo(Long id){
+        articuloRepository.deleteById(id);
     }
 }
