fb14ac1d2074f737cee86205372eff3004889032
Vania <Van.carvajal@duocuc.cl>
2025-12-01 02:13:46 -0300

    Actualización

--- src/main/java/com/levelupgamer/backend/controller/CarritoController.java
+++ src/main/java/com/levelupgamer/backend/controller/CarritoController.java
@@ -8,15 +8,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.levelupgamer.backend.model.Carrito;
+import com.levelupgamer.backend.service.CarritoService;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import com.levelupgamer.backend.model.Carrito;
-import com.levelupgamer.backend.service.CarritoService;
 
 
 @RestController
@@ -32,18 +33,6 @@ public class CarritoController {
         return new ResponseEntity<>(nuevaCarrito, HttpStatus.CREATED);
     }
     
-    @PostMapping("/finalizar-compra")
-    public ResponseEntity<?> finalizarCompra(@RequestBody Carrito carrito) {
-        try {
-            Carrito ordenFinalizada = carritoService.finalizarCompra(carrito);
-            return new ResponseEntity<>(ordenFinalizada, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body("Error al finalizar la compra: " + e.getMessage());
-        }
-    }
-
     @GetMapping
     public List<Carrito> obtenerTodas() {
         return carritoService.obtenerTodas();
--- src/main/java/com/levelupgamer/backend/service/CarritoService.java
+++ src/main/java/com/levelupgamer/backend/service/CarritoService.java
@@ -2,50 +2,19 @@ package com.levelupgamer.backend.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.type.TypeReference;
 
 import com.levelupgamer.backend.model.Carrito;
 import com.levelupgamer.backend.repository.CarritoRepository;
 
-@Transactional
 @Service
 public class CarritoService {
 
     @Autowired
     private CarritoRepository carritoRepository;
-    
-    
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    public Carrito finalizarCompra(Carrito carrito) throws Exception {
-        
-        try {
-            List<Map<String, Object>> items = objectMapper.readValue(
-                carrito.getItemsJson(), 
-                new TypeReference<List<Map<String, Object>>>() {}
-            );
-
-            for (Map<String, Object> item : items) {
-                Long productoId = ((Number) item.get("codigo")).longValue(); 
-                Integer cantidad = (Integer) item.get("quantity"); 
-                System.out.println("LOGICA DE NEGOCIO EJECUTADA: Reduciendo stock del Producto ID: " + productoId + " en cantidad: " + cantidad);
-
-            }
-        } catch (Exception e) {
-            throw new Exception("Error al procesar la compra o stock insuficiente: " + e.getMessage());
-        }
-        
-        carrito.setFechaCompra(LocalDateTime.now()); 
-        return carritoRepository.save(carrito);
-    }
 
     public Carrito guardarCarrito(Carrito carrito){
         carrito.setFechaCompra(LocalDateTime.now()); 
@@ -61,14 +30,14 @@ public class CarritoService {
     }
 
     public Carrito actualizaCarrito(Long id, Carrito detallesCarrito){
-         return carritoRepository.findById(id).map(carritoExistente -> {
-             carritoExistente.setSubtotal(detallesCarrito.getSubtotal());
-             carritoExistente.setItemsJson(detallesCarrito.getItemsJson());
-             carritoExistente.setCostoEnvio(detallesCarrito.getCostoEnvio());
-             carritoExistente.setTotalPagar(detallesCarrito.getTotalPagar());
-             carritoExistente.setFechaCompra(detallesCarrito.getFechaCompra());
-             return carritoRepository.save(carritoExistente);
-         }).orElse(null);
+        return carritoRepository.findById(id).map(carritoExistente -> {
+            carritoExistente.setSubtotal(detallesCarrito.getSubtotal());
+            carritoExistente.setItemsJson(detallesCarrito.getItemsJson());
+            carritoExistente.setCostoEnvio(detallesCarrito.getCostoEnvio());
+            carritoExistente.setTotalPagar(detallesCarrito.getTotalPagar());
+            carritoExistente.setFechaCompra(detallesCarrito.getFechaCompra());
+            return carritoRepository.save(carritoExistente);
+        }).orElse(null);
     }
 
     public void eliminarCarrito(Long id){
